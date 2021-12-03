9a9b2ee3ac82eb0607354b875ffd7c28538d099d
rinabang <91496352+rinabang@users.noreply.github.com>
2021-12-03 13:13:34 +0300

    Update LinkedListWork.java

--- lab3/src/lab3/LinkedListWork.java
+++ lab3/src/lab3/LinkedListWork.java
@@ -5,14 +5,12 @@ import java.util.List;
  * Класс, вызывающий основные методы коллекции LinkedList
  * @author Суховеркова Екатерина (3 курс 3 группа)
 */
-public class LinkedListWork {
-	/** Поле LinkedList */
-	public List<Integer> linkedList;
+public class LinkedListWork extends ListWork{
 	/**
      * Конструктор LinkedList
      */
     public LinkedListWork() {
-        linkedList = new LinkedList<>();
+        list = new LinkedList<>();
     }
     /**
      * Метод заполнения LinkedList
@@ -22,37 +20,7 @@ public class LinkedListWork {
     public long fillLinkedList(int count) {
         var startTime = System.nanoTime();
         for(int i = 0; i < count; i++)
-        	linkedList.add(i);
-        return System.nanoTime() - startTime;
-    }
-    /**
-     * Метод удаления элемента для LinkedList
-     * @param index - индекс элемента для удаления
-     * @return возвращает время, необходимое для удаления элемента
-     */
-    public long delete(int index) {
-        var startTime = System.nanoTime();
-        linkedList.remove(index);
-        return System.nanoTime() - startTime;
-    }
-    /**
-     * Метод добавления элемента для LinkedList
-     * @param index - индекс элемента для добавления
-     * @return возвращает время, необходимое для добавления элемента
-     */
-    public long add(int index) {
-        var startTime = System.nanoTime();
-        linkedList.add(index,1);
-        return System.nanoTime() - startTime;
-    }
-    /**
-     * Метод получения элемента для LinkedList
-     * @param index - индекс элемента для получения
-     * @return возвращает время, необходимое для получения элемента
-     */
-    public long get(int index) {
-        var startTime = System.nanoTime();
-        int item = linkedList.get(index);
+        	list.add(i);
         return System.nanoTime() - startTime;
     }
 }
