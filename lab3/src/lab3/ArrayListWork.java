37bf7f3178df3b3dfcbafb4e22d1be1658d8c3e9
rinabang <91496352+rinabang@users.noreply.github.com>
2021-12-03 13:12:52 +0300

    Update ArrayListWork.java

--- lab3/src/lab3/ArrayListWork.java
+++ lab3/src/lab3/ArrayListWork.java
@@ -5,9 +5,7 @@ import java.util.List;
  * Класс, вызывающий основные методы коллекции ArrayList
  * @author Суховеркова Екатерина (3 курс 3 группа)
 */
-public class ArrayListWork {
-	/** Поле ArrayList */
-	public List<Integer> arrayList;
+public class ArrayListWork extends ListWork {
 	/** Поле размера arrayList */
     private int size;
     /**
@@ -16,7 +14,7 @@ public class ArrayListWork {
      */
     public ArrayListWork(int s) {
         size = s;
-        arrayList = new ArrayList<>(size);
+        list = new ArrayList<>(size);
     }
     /**
      * Метод заполнения ArrayList
@@ -25,37 +23,7 @@ public class ArrayListWork {
     public long fillArrayList() {
         var startTime = System.nanoTime();
         for (int i = 0; i < size; i++)
-            arrayList.add(i);
-        return System.nanoTime() - startTime;
-    }
-    /**
-     * Метод удаления элемента для ArrayList
-     * @param index - индекс элемента для удаления
-     * @return возвращает время, необходимое для удаления элемента
-     */
-    public long delete(int index) {
-        var startTime = System.nanoTime();
-        arrayList.remove(index);
-        return System.nanoTime() - startTime;
-    }
-    /**
-     * Метод добавления элемента для ArrayList
-     * @param index - индекс элемента для добавления
-     * @return возвращает время, необходимое для добавления элемента
-     */
-    public long add(int index) {
-        var startTime = System.nanoTime();
-        arrayList.add(index, 1);
-        return System.nanoTime() - startTime;
-    }
-    /**
-     * Метод получения элемента для ArrayList
-     * @param index - индекс элемента для получения
-     * @return возвращает время, необходимое для получения элемента
-     */
-    public long get(int index) {
-        var startTime = System.nanoTime();
-        int item = arrayList.get(index);
+            list.add(i);
         return System.nanoTime() - startTime;
     }
 }
